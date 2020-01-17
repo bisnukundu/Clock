5fa0b96ec3e456a6e404d4396a19e3f28c0cfbf2
bisnukundu <bisnkundu@gmail.com>
2020-01-17 23:09:44 +0600

    Data 1/17/2020 & Time 11:09

--- javascript.js
+++ javascript.js
@@ -1,32 +1,24 @@
+
 let second = document.querySelector(".second");
 let minute = document.querySelector(".minutes");
 let hour = document.querySelector(".hours");
+let date = new Date();
 let se = 6;
 let mi = 6;
 let hou = 30;
 
-function test() {
-	let date = new Date();
-	let hours = date.getHours()
-	let minutes = date.getMinutes()
+// Seconds 
+function sec() {
 	let seconds = date.getSeconds()
-	let final = hours + ":" + minutes + ":" + seconds
-	// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 	let s = `transform:rotate(${se}deg)`
 	se += 6
 	second.style = s
 }
 
-setInterval(test, 1000)
-// ================================================
-
+setInterval(sec, 1000)
+// Minutes 
 function min() {
-	let date = new Date();
-	let hours = date.getHours()
 	let minutes = date.getMinutes()
-	let seconds = date.getSeconds()
-	let final = hours + ":" + minutes + ":" + seconds
-	// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 	let m = `transform:rotate(${mi}deg)`
 	mi += 6
 	minute.style = m
@@ -34,18 +26,12 @@ function min() {
 
 setInterval(min, 60000)
 
-// ======================================================
-
+// Hours 
 function houe() {
-	let date = new Date();
 	let hours = date.getHours()
-	let minutes = date.getMinutes()
-	let seconds = date.getSeconds()
-	let final = hours + ":" + minutes + ":" + seconds
-	// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 	let h = `transform:rotate(${hou}deg)`
-	hou += 6
+	hou += 30
 	hour.style = h
 }
 
-setInterval(houe, 3600000)
+setInterval(houe,3600000)
