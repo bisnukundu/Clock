9d7bf816aaed94b251e3550a0e906f51da743401
bisnukundu <bisnkundu@gmail.com>
2020-01-18 17:25:15 +0600

    Date 1/18/2020 & Time 5:25PM

--- javascript.js
+++ javascript.js
@@ -1,37 +1,23 @@
-
-let second = document.querySelector(".second");
-let minute = document.querySelector(".minutes");
-let hour = document.querySelector(".hours");
-let date = new Date();
-let se = 6;
-let mi = 6;
+let second = document.querySelector(".second")
+let hour = document.querySelector(".hours")
+let minute = document.querySelector(".minutes")
+let sec = 6;
+let min = 6;
 let hou = 30;
+// Seconds  
+setInterval(function(){
+	second.style =`transform:rotate(${sec}deg)`;
+	sec += 6;
+},1000)
 
-// Seconds 
-function sec() {
-	let seconds = date.getSeconds()
-	let s = `transform:rotate(${se}deg)`
-	se += 6
-	second.style = s
-}
-
-setInterval(sec, 1000)
-// Minutes 
-function min() {
-	let minutes = date.getMinutes()
-	let m = `transform:rotate(${mi}deg)`
-	mi += 6
-	minute.style = m
-}
-
-setInterval(min, 60000)
+//Minutes
+setInterval(function(){
+	minute.style =`transform:rotate(${min}deg)`;
+	min += 6;
+},60000)
 
 // Hours 
-function houe() {
-	let hours = date.getHours()
-	let h = `transform:rotate(${hou}deg)`
-	hou += 30
-	hour.style = h
-}
-
-setInterval(houe,3600000)
+setInterval(function(){
+	hour.style =`transform:rotate(${hou}deg)`;
+	hou += 30;
+},3600000)
